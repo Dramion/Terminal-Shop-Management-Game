72c98b107b1c6d104572f9cdd60e2f1b0f6d8984
Dramion <43044187+Dramion@users.noreply.github.com>
2023-10-17 18:53:39 +0000

    Documentation and test code.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-
-
+"""Program initializer"""
 
 if __name__ == "__main__":
-    import classes
+    import src.scripts.classes as classPy
+    store = classPy.Store(
+        input("What would you like the name of your store to be?"), 30)
+
+    print(store)
--- src/scripts/classes.py
+++ src/scripts/classes.py
@@ -1,37 +1,50 @@
+"""Module containing short object classes until they become larger."""
+
+
 class Store:
+    """Class for defining a store."""
     # Inv Dictionary format: {item_name: {"price": item_price, "quantity": item_quantity}, ...}
     inventory = {}
+
     def __init__(self, store_name, store_funds):
         self.name = store_name
         self.balance = store_funds
 
     def __repr__(self):
-        return "{} has an inventory of {}, and a total balance of ${}.".format(self.name, self.inventory, self.balance)
+        return f"{self.name} has an inventory of {self.inventory}, and a total balance\
+            of ${self.balance}."
 
     def add_inv(self, item):
-        if type(item) is Item and item.name in self.inventory:
+        """Method for adding items to the store's inventory."""
+        if isinstance(item) is Item and item.name in self.inventory:
             self.inventory[item.name]["quantity"] += item.dict[item.name]["quantity"]
-        elif type(item) is Item and item.name not in self.inventory:
+        elif isinstance(item) is Item and item.name not in self.inventory:
             self.inventory.update(item.dict)
             self.inventory[item.name]["quantity"] = 1
         else:
-            print("'{}' is not a valid item.".format(item))
+            print(f"'{item}' is not a valid item.")
+
 
 class Item:
+    """Class for defining an item."""
     def __init__(self, item_name, item_price, item_quantity=1):
         self.name = item_name
         self.price = item_price
-        self.dict = {self.name: {"price": self.price, "quantity": item_quantity}}
+        self.dict = {self.name: {
+            "price": self.price, "quantity": item_quantity}}
 
     def __repr__(self):
-        return "{} costs ${} per item.".format(self.name, self.price)
+        return "f{self.name} costs ${self.price} per item."
+
 
 class Customer:
+    """Class for defining a customer."""
     cart = {}
 
-    def __init__(self, customer_name, customer_balance):
+    def __init__(self, customer_name, customer_balance, store):
         self.name = customer_name
         self.balance = customer_balance
+        self.location = store
 
     def __repr__(self):
-        return "{} has ${} in their wallet and {} in their cart.".format(self.name, self.balance, self.cart)
+        return f"{self.name} has ${self.balance} in their wallet and {self.cart} in their cart."
